a0908aa9da01f7194381ea768202efef85b203d9
Koh Hui Soon <huisoonkoh@gmail.com>
2022-08-10 22:07:39 +0800

    Tested Parameters

--- src/main/java/commkmeans/util/Parameters.java
+++ src/main/java/commkmeans/util/Parameters.java
@@ -12,6 +12,42 @@ public class Parameters {
     public Parameters(
             double T_max, double T_min, int N_max, int N_min,
             double alpha, int R, int seed) {
+        if (T_max <= 0 || T_min <= 0) {
+            String msg = String.format(
+                    "T must be positive, received %f, %f instead.", T_min, T_max);
+            throw new IllegalArgumentException(msg);
+        }
+
+        if (T_max <= T_min) {
+            String msg = String.format(
+                    "T_max must be strictly greater than T_min, received %f, %f instead.", T_min, T_max);
+            throw new IllegalArgumentException(msg);
+        }
+
+        if (N_max <= 0 || N_min <= 0) {
+            String msg = String.format(
+                    "N must be positive, received %d, %d instead.", N_min, N_max);
+            throw new IllegalArgumentException(msg);
+        }
+
+        if (N_max < N_min) {
+            String msg = String.format(
+                    "N_max must be greater than N_min, received %d, %d instead.", N_min, N_max);
+            throw new IllegalArgumentException(msg);
+        }
+
+        if (alpha <= 0.0 || alpha >= 1.0) {
+            String msg = String.format(
+                    "alpha must be in (0.0, 1.0), received %f instead.", alpha);
+            throw new IllegalArgumentException(msg);
+        }
+
+        if (R <= 0) {
+            String msg = String.format(
+                    "R must be positive, received %d instead.", R);
+            throw new IllegalArgumentException(msg);
+        }
+
         this.T_max = T_max;
         this.T_min = T_min;
         this.N_max = N_max;
--- src/test/java/commkmeans/util/ParametersTest.java
+++ src/test/java/commkmeans/util/ParametersTest.java
@@ -1,7 +1,138 @@
 package commkmeans.util;
 
+import org.junit.jupiter.api.Test;
+
+import static commkmeans.util.Constants.EPSILON;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParametersTest {
+    @Test
+    void ctor_shouldCreateWithCorrectValues() {
+        double T_max = 100.0;
+        double T_min = 0.5;
+        int N_max = 100000;
+        int N_min = 2;
+        double alpha = 0.5;
+        int R = 1000;
+        int seed = 300;
 
+        Parameters p = new Parameters(T_max, T_min, N_max, N_min, alpha, R, seed);
+
+        assertEquals(T_max, p.T_max, EPSILON);
+        assertEquals(T_min, p.T_min, EPSILON);
+        assertEquals(N_max, p.N_max);
+        assertEquals(N_min, p.N_min);
+        assertEquals(alpha, p.alpha, EPSILON);
+        assertEquals(R, p.R);
+        assertEquals(seed, p.seed);
+    }
+
+    @Test
+    void ctor_shouldCreateWithCorrectValues2() {
+        double T_max = 100.0;
+        double T_min = 0.5;
+        int N_max = 100000;
+        int N_min = 2;
+        double alpha = 0.5;
+        int R = 1000;
+
+        Parameters p = new Parameters(T_max, T_min, N_max, N_min, alpha, R);
+
+        assertEquals(T_max, p.T_max, EPSILON);
+        assertEquals(T_min, p.T_min, EPSILON);
+        assertEquals(N_max, p.N_max);
+        assertEquals(N_min, p.N_min);
+        assertEquals(alpha, p.alpha, EPSILON);
+        assertEquals(R, p.R);
+        assertEquals(-1, p.seed);
+    }
+
+    @Test
+    void ctor_nonPegativeTShouldThrowIAE() {
+        int N_max = 100000;
+        int N_min = 2;
+        double alpha = 0.5;
+        int R = 1000;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(-1.0, 1.0, N_max, N_min, alpha, R));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(1.0, -1.0, N_max, N_min, alpha, R));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(0.0, 1.0, N_max, N_min, alpha, R));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(1.0, 0.0, N_max, N_min, alpha, R));
+    }
+
+    @Test
+    void ctor_TminLargerThanEqualToTmaxShouldThrowIAE() {
+        int N_max = 100000;
+        int N_min = 2;
+        double alpha = 0.5;
+        int R = 1000;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(1.0, 2.0, N_max, N_min, alpha, R));
+    }
+
+    @Test
+    void ctor_nonPositiveNShouldThrowIAE() {
+        double T_max = 2.0;
+        double T_min = 1.0;
+        double alpha = 0.5;
+        int R = 1000;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, -1, -5, alpha, R));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, 2, -1, alpha, R));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, 2, 0, alpha, R));
+    }
+
+    @Test
+    void ctor_NminLargerThanNmaxShouldThrowIAE() {
+        double T_max = 2.0;
+        double T_min = 1.0;
+        double alpha = 0.5;
+        int R = 1000;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, 2, 3, alpha, R));
+    }
+
+    @Test
+    void ctor_AlphaNotBetweenZeroAndOneShouldThrowIAE() {
+        double T_max = 2.0;
+        double T_min = 1.0;
+        int N_max = 10000;
+        int N_min = 2;
+        int R = 1000;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, N_max, N_min, -0.01, R));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, N_max, N_min, 1.01, R));
+    }
+
+    @Test
+    void ctor_nonPositiveRShouldThrowIAE() {
+        double T_max = 2.0;
+        double T_min = 1.0;
+        int N_max = 10000;
+        int N_min = 2;
+        double alpha = 0.5;
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, N_max, N_min, alpha, -1));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new Parameters(T_max, T_min, N_max, N_min, alpha, 0));
+    }
 }
